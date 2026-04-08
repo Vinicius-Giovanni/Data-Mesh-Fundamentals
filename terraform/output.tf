b903f85e25d55dbc1902b07d7c74c797abc88ace
Vinicius-Giovanni <viniciusgiovanni2003@gmail.com>
2026-04-08 14:31:30 -0300

    Infraestrutura com Terraform Criando banco de dados com RDS da amazon, e buckets no S3 também da Amazon

---
+++ terraform/output.tf
@@ -0,0 +1,14 @@
+output "rds_endpoint" {
+    description = "Endpoint da instância RDS PostgreSQL"
+    value = aws_db_instance.postgres.endpoint
+    }
+
+output "s3_bucket_name" {
+    description = "Nome do bucket s3"
+    value = aws_s3_bucket.crm_bucket.bucket
+    }
+
+output "vpc_id" {
+    description = "ID da VPC"
+    value = "aws_vpc.main.id
+    }
